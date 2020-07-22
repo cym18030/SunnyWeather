a3ccc510b69b17f2b84f886bd86c627967611591
cym18030 <cym18030@gmail.com>
2020-07-22 14:14:03 +0800

    实现搜索全球城市数据功能。

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -6,6 +6,9 @@ import android.content.Context
 
 class SunnyWeatherApplication : Application() {
     companion object {
+        // 彩云天气Token
+        const val TOKEN = "Zq53YLDwk0bAhnyD"
+
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
     }
